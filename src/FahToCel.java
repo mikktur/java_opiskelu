efd9af32bb5a82ca1d8e722057324f4bd77ccab1
mikktur <mikko.silenceri@gmail.com>
2023-12-20 23:37:55 +0200

    edit

---
+++ src/FahToCel.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FahToCel {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input degrees in fahrenheit");
+        double fah = Double.parseDouble(scanner.nextLine());
+        double cel = (fah-32)*5/9;
+        System.out.printf("%.2f Fahrenheit in Celsius is: %.2f%n", fah, cel);
+    }
+}
