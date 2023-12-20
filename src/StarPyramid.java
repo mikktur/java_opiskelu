b6614cfb7cfa7fa2a0a13be6d1e2554e6b4296c1
mikktur <mikko.silenceri@gmail.com>
2023-12-20 22:03:06 +0200

    Initial commit

---
+++ src/StarPyramid.java
@@ -0,0 +1,8 @@
+public class StarPyramid {
+    public static void main(String[]args){
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+    }
+}
---
+++ src/SumOfThreeNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfThreeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("The product of the numbers is " + (first * second * third));
+        //had to change int to float, since averages are usually float numbers.
+        System.out.println("The average of the numbers is " + ((float)(first + second + third) / 3));
+    }
+}
