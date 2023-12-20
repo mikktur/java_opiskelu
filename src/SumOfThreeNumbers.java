56f89931bfb2639b06a2ab8f501bd8e78818e7f0
mikktur <mikko.silenceri@gmail.com>
2023-12-20 22:52:50 +0200

    edit

--- src/SumOfThreeNumbers.java
+++ src/SumOfThreeNumbers.java
@@ -16,9 +16,11 @@ public class SumOfThreeNumbers {
 
         System.out.println("The sum of the numbers is " + (first + second + third));
         System.out.println("The product of the numbers is " + (first * second * third));
-        /* I had to change the print output to float, as averages are typically expressed as
-         floating-point numbers. In this case, it was rounded to the closest whole number, resulting in 3 instead of 3.3333333.
+        /* I had to change the print output to double, as averages are typically expressed as
+         floating-point numbers. In this case, it was rounded to the closest whole number as integer, resulting in 3 instead of 4.333333333333333.
+         and float was not accurate enough, so it needed to be a double.
+          Also, i limited decimals, so it should be exactly the same number now.
          */
-        System.out.println("The average of the numbers is " + ((float)(first + second + third) / 3));
+        System.out.printf("The average of the numbers is %.13f%n", ((double)(first + second + third) / 3));
     }
 }
