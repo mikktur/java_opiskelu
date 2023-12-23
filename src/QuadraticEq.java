3d687627cd0fa159e0b8e09e21429021ecb9dccb
mikko <mikko.silenceri@gmail.com>
2023-12-24 00:38:45 +0200

    quadratic equation

---
+++ src/QuadraticEq.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class QuadraticEq {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Give the value of a: ");
+        double a = Double.parseDouble(scanner.nextLine());
+        System.out.print("Give the value of b: ");
+        double b = Double.parseDouble(scanner.nextLine());
+        System.out.print("Give the value of c: ");
+        double c = Double.parseDouble(scanner.nextLine());
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        double root;
+        double root2;
+        if (a == 0) {
+            if (b != 0) {
+                root = -c / b;
+                System.out.printf("The root is: %.2f%n", root);
+            } else if (c == 0) {
+                System.out.println("Infinite solutions every real number is a solution.");
+            } else {
+                System.out.println("No solution.");
+            }
+        } else if (discriminant < 0) {
+            System.out.print("No real roots");
+
+        } else if (discriminant > 0) {
+            root = (-b + Math.sqrt(discriminant)) / (2 * a);
+            root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.printf("The roots are : %.2f and %.2f", root, root2);
+
+        } else {
+            root = -b / (2 * a);
+            System.out.printf("The root is : %.2f", root);
+
+        }
+        //b+-sqrt(b^2-4*a*c)/2a//
+
+
+    }
+}
