059a8d47d2191a0990fd25a81785a049c6fdf0be
mikktur <mikko.silenceri@gmail.com>
2024-01-27 13:43:39 +0200

    Array tasks

---
+++ src/T_2_1_4/TelevisionViewer.java
@@ -0,0 +1,11 @@
+package T_2_1_4;
+
+public class TelevisionViewer {
+    public static void main(String[]args){
+        Televisio televisio = new Televisio();
+        for (int i = 0;i<10;i++) {
+            televisio.watch();
+        }
+    }
+
+}
